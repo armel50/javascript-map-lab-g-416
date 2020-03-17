f7b5ea4bbabdfdf296b5badc4b761a020d8bd958
Cloe Kouadjo <armelfli50@gmail.com>
2020-03-17 05:14:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const sortedRobots = robots.map(e => {
+  let result = {}
+   if(knownDecepticons.includes(e.name) ){
+     result = Object.assign({}, e, {
+    alliance:"decepticon"
+  });
+     }else{
+          result = Object.assign({}, e, {
+    alliance:"autobots"
+  });
+     }
+   return result
+})
